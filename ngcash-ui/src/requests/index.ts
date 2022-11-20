d49ba78b75eb6d3099e53d7da3b45bba40d62ca2
Abner <abnerferreira84@gmail.com>
2022-11-20 10:01:50 -0300

    feat: add arquivo com funções de requisições a API.

--- ngcash-ui/src/requests/index.ts
+++ ngcash-ui/src/requests/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Token, User } from '../interfaces';
+import { Token, Transaction, User } from '../interfaces';
 
 const URL = 'http://localhost:3001';
 
@@ -7,7 +7,7 @@ export const login = async (username: string, password: string)
 : Promise<Token | void> => {
     try {
         const response = await axios.post(`${URL}/login`, { username, password})
-        return await response.data;
+        return response.data;
     } catch(err) {
         console.log(err)
     }
@@ -17,7 +17,17 @@ export const register = async (username: string, password: string)
 : Promise<User | void> => {
     try {
         const response = await axios.post(`${URL}/user`, { username, password})
-        return await response.data;
+        return response.data.user;
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+export const fetchTransactions = async (token: string | null)
+: Promise<Transaction[] | [] | void> => {
+    try {
+        const response = await axios.get(`${URL}/transaction`)
+        return response.data;
     } catch(err) {
         console.log(err)
     }
